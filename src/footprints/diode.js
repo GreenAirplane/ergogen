d9ab829a272dc185eba3169af84331beaa03d2f7
GreenAirplane <alex.s.shashkin@gmail.com>
2022-06-14 05:58:44 -0400

    Added settings for front, back diode pads and tht renders

--- src/footprints/diode.js
+++ src/footprints/diode.js
@@ -4,10 +4,13 @@ module.exports = {
         to: undefined
     },
     params: {
-        class: 'D'
+        class: 'D',
+        tht: true,
+        front: true,
+        back: true
     },
-    body: p => `
-  
+    body: p => {
+        const base = `
     (module ComboDiode (layer F.Cu) (tedit 5B24D78E)
 
 
@@ -32,17 +35,26 @@ module.exports = {
         (fp_line (start -0.35 0) (end -0.35 0.55) (layer B.SilkS) (width 0.1))
         (fp_line (start -0.35 0) (end -0.35 -0.55) (layer B.SilkS) (width 0.1))
         (fp_line (start -0.75 0) (end -0.35 0) (layer B.SilkS) (width 0.1))
-    
-        ${''/* SMD pads on both sides */}
-        (pad 1 smd rect (at -1.65 0 ${p.rot}) (size 0.9 1.2) (layers F.Cu F.Paste F.Mask) ${p.net.to.str})
-        (pad 2 smd rect (at 1.65 0 ${p.rot}) (size 0.9 1.2) (layers B.Cu B.Paste B.Mask) ${p.net.from.str})
-        (pad 1 smd rect (at -1.65 0 ${p.rot}) (size 0.9 1.2) (layers B.Cu B.Paste B.Mask) ${p.net.to.str})
-        (pad 2 smd rect (at 1.65 0 ${p.rot}) (size 0.9 1.2) (layers F.Cu F.Paste F.Mask) ${p.net.from.str})
-        
+        `
+        function pads(def_side) {
+            return `
+            ${''/* SMD pads*/}
+            (pad 1 smd rect (at -1.65 0 ${p.rot}) (size 0.9 1.2) (layers ${def_side}.Cu ${def_side}.Paste ${def_side}.Mask) ${p.net.to.str})
+            (pad 2 smd rect (at 1.65 0 ${p.rot}) (size 0.9 1.2) (layers ${def_side}.Cu ${def_side}.Paste ${def_side}.Mask) ${p.net.from.str})    
+            `
+        }
+
+        const tht = `        
         ${''/* THT terminals */}
         (pad 1 thru_hole circle (at 3.81 0 ${p.rot}) (size 1.905 1.905) (drill 0.9906) (layers *.Cu *.Mask) ${p.net.from.str})
         (pad 2 thru_hole rect (at -3.81 0 ${p.rot}) (size 1.778 1.778) (drill 0.9906) (layers *.Cu *.Mask) ${p.net.to.str})
-    )
-  
-    `
+        `
+
+        return `
+            ${base}
+            ${p.param.front ? pads('F') : ''}
+            ${p.param.back ? pads('B') : ''}
+            ${p.param.tht ? tht : ''})
+        `
+    }
 }
